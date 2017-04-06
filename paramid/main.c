f65a08dcf2495266d9b5daa26f6f036b0f93c045
Mahesh kumar <maheshlantech@gmail.com>
2017-04-06 14:04:14 +0530

    Initial Commit

---
+++ paramid/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  paramid
+//
+//  Created by Mahesh  Mishra on 06/04/17.
+//  Copyright © 2017 Mahesh  Mishra. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
